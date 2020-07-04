abfbbdc16385f1e0813acd9e70b4e7f99afe5a46
Jakub Pawelec <mkbewe1112@gmail.com>
2020-07-04 21:47:29 +0200

    BookStore commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> option) : base(option)
+        {
+
+        }
+
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ Models/Books.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookShop.Model
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public String Name { get; set; }
+        [Required]
+        public String Author { get; set; }
+        
+        public DateTime Year_of_realesae { get; set; }
+
+        public int Sites { get; set; }
+
+        public int Quantity { get; set; }
+
+    }
+}
